ebb11c0cf1c7d9321171dec98f8a8cd2bcbb73a9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-04 23:27:05 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -4,4 +4,14 @@ class Admin::PreferencesController < ApplicationController
     @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true) 
   end 
   
+  def update 
+    
+  end 
+  
+  private
+  
+   def preference_params
+     
+   end 
+  
 end 
