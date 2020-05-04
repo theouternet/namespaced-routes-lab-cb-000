2985a2e75e10688184975833b638faf412891443
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-04 23:31:44 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -5,13 +5,16 @@ class Admin::PreferencesController < ApplicationController
   end 
   
   def update 
-    
+    @preference = Preference.find(params[:id])
+    @preference.update(preference_params)
+    redirect_to admin_preferences_path
+  end
   end 
   
   private
   
    def preference_params
-     
+     params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
    end 
   
 end 
